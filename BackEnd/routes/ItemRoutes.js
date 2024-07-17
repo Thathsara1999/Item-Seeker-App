364da14dc28da67895fbd0dea8079633cd27ee67
L A D N Thathsara <141746388+Thathsara1999@users.noreply.github.com>
2024-07-17 21:24:53 +0530

    Update ItemRoutes.js

--- BackEnd/routes/ItemRoutes.js
+++ BackEnd/routes/ItemRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../utills/multer'); // Adjust path based on your project structure
-const { saveItem, getItems } = require('../controllers/ItemController');
+const { saveItem, getItems, deleteItem ,getItemById } = require('../controllers/ItemController');
 
 // Route to save a new item
-router.post('/post/save', upload.single('image'), saveItem);
-
+router.post('/post/save', upload.array('images', 10), saveItem); // Allow up to 10 images
 // Route to get all items
 router.get('/items', getItems);
+//Route to delete Items
+router.delete('/items/:id',deleteItem);
+//Route to getItemByID
+router.get('/items/:id',getItemById);
 
 module.exports = router;
