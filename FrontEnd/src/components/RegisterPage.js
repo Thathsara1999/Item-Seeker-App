e52352ca30778043a59a1ae9035ff99519a7266c
L A D N Thathsara <141746388+Thathsara1999@users.noreply.github.com>
2024-07-17 21:49:49 +0530

    Update RegisterPage.js

--- FrontEnd/src/components/RegisterPage.js
+++ FrontEnd/src/components/RegisterPage.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { registerUser } from './API';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import './CSS_Files/Signup.css';
 
 const RegisterPage = () => {
-  const navigate = useNavigate(); // Initialize useNavigate hook
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -20,11 +21,10 @@ const RegisterPage = () => {
         username,
         email,
         password,
-        confirmPassword,
       };
       await registerUser(userData);
       alert('Registration successful');
-      navigate('/'); // Use navigate to go to login page after successful registration
+      navigate('/');
     } catch (error) {
       console.error('Error registering user:', error);
       alert('Error registering user');
@@ -32,38 +32,58 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="register-page">
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
+    <div className='reg-background'>
+      <form onSubmit={handleSubmit} className='reg-form'>
+        <h1 className='login-font'>Item Seeker</h1>
+        <div className='login-padding'></div>
+        <label className='login-label'>Name</label><br />
         <input
           type="text"
+          className='reg-textbox'
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Username"
           required
-        />
+        />   <div className='login-padding'></div>
+     
+        <label className='login-label' style={{ marginBottom: "1px" }}>Email</label><br />
         <input
           type="email"
+          className='reg-textbox'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
           required
-        />
+        />   
+        <div className='login-padding'></div>
+        <label className='login-label' style={{ marginBottom: "1px" }}>Password</label><br />
         <input
           type="password"
+          className='reg-textbox'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
           required
-        />
+        />   <div className='login-padding'></div>
+     
+        <label className='login-label' style={{ marginBottom: "1px" }}>Re-Enter Password</label><br />
         <input
           type="password"
+          className='reg-textbox'
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
           placeholder="Confirm Password"
           required
-        />
-        <button type="submit">Register</button>
+        />   <div className='login-padding'></div>
+        <div className='login-padding'></div>
+        <button type='submit' className='reg-signup'>Sign up</button>
+        <div className='login-padding'></div>
+        
+    
+        
+   
+        <label className='login-font'>Do you have an account?</label>
+        <Link to='/'><label className='reg-forgot'>Sign in</label></Link>
       </form>
     </div>
   );
